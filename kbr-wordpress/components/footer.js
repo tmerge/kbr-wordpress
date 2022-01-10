4c38d0a081a4efc7c1c29ed4054ff458db2c3190
tmerge <tmerge@uos.de>
2022-01-11 00:02:24 +0100

    #2 make footer responsive

--- kbr-wordpress/components/footer.js
+++ kbr-wordpress/components/footer.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Footer() {
     return (
         <footer className="border-t pt-5 pb-5 text-gray-500">
-            <div className="menu ml-32 flex flex-row">
+            <div className="menu sm:ml-32 text-center block sm:flex sm:flex-row">
                 <p className="mr-2">© {new Date().getFullYear()} KBR |</p>
                 <Link href="/impressum"><a className="mr-2">Impressum |</a></Link>
                 <Link href="/impressum"><a>Datenschutz</a></Link>
