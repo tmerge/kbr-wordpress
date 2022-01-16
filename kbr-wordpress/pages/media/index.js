34002e1ef58a1ac7e8aff886be2608ebca3f852a
tmerge <tmerge@uos.de>
2022-01-17 00:04:25 +0100

    first attempt of audio player

--- kbr-wordpress/pages/media/index.js
+++ kbr-wordpress/pages/media/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import ReactAudioPlayer from 'react-audio-player'
 import Layout from '../../components/layout'
 
 export default function index() {
+    
     return (
         <Layout>
             <h1>Medienportal</h1>
+            <ReactAudioPlayer src="/audio/podcast.mp3" controls="true" />
         </Layout>
         
     )
